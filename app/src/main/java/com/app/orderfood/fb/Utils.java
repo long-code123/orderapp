b78d19bc6eb6866f793b3afde977ca9c209056dd
Phạm Trường <61191253+truongpa28@users.noreply.github.com>
2024-04-27 15:22:15 +0700

    push code

--- app/src/main/java/com/app/orderfood/fb/Utils.java
+++ app/src/main/java/com/app/orderfood/fb/Utils.java
@@ -1,9 +1,16 @@
 package com.app.orderfood.fb;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Utils {
 
     public static long getNewId() {
         return System.currentTimeMillis();
+    }
+
+    public static String getUID() {
+        return "mw2lJ6D40pOiPHoZCNd4GpQ2UHy2";
+        //return FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
 }
