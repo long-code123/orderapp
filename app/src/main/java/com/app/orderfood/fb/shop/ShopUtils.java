b338856b2f083ce9fda7d8f7eb4bfe0960cf25e2
Asus <nguyenbalong250902@gmail.com>
2024-04-29 16:41:15 +0700

    User Information

--- app/src/main/java/com/app/orderfood/fb/shop/ShopUtils.java
+++ app/src/main/java/com/app/orderfood/fb/shop/ShopUtils.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.app.orderfood.models.Category;
+import com.app.orderfood.models.Foods;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -18,6 +19,7 @@ import java.util.ArrayList;
 public class ShopUtils {
 
     private static final String CATEGORY = "Category";
+    private static final String FOOD = "Foods";
 
     static FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
 
@@ -147,5 +149,124 @@ public class ShopUtils {
                     }
                 });
     }
+
+    public static void insertFood(Foods food) {
+
+        firebaseDatabase.getReference(FOOD)
+                .child(String.valueOf(food.getId()))
+                .setValue(food)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //ok
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //fail
+                    }
+                });
+    }
+    public static void getFoodById(long id) {
+        firebaseDatabase.getReference(FOOD)
+                .child(String.valueOf(id))
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task1) {
+                        if (task1.isSuccessful()) {
+
+                            DataSnapshot result1 = task1.getResult();
+
+                            if (result1.exists()) {
+                                Foods foods = result1.getValue(Foods.class);
+                                Log.e("truongpa", "lấy dc giữ liệu có: " + foods.getTitle());
+                            } else {
+                                Log.e("truongpa", "lấy dc giữ không có: ");
+                            }
+                        } else {
+                            Log.e("truongpa", "lấy dc giữ không thành công");
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("truongpa", "lỗi kết lỗi");
+                    }
+                });
+    }
+    public static void getAllFood() {
+        firebaseDatabase.getReference(FOOD)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot1) {
+
+                        ArrayList<Foods> arrayList = new ArrayList<>();
+
+                        for (DataSnapshot item : snapshot1.getChildren()) {
+                            Foods itemData1 = item.getValue(Foods.class);
+
+                            arrayList.add(itemData1);
+                        }
+
+                        Log.e("truongpa", "getAllFood - Success");
+
+                        for (Foods foods : arrayList) {
+                            Log.e("truongpa", "" + foods.getId() + " - " + foods.getTitle());
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.e("truongpa", "getAllFood - Fail");
+                    }
+                });
+
+    }
+    public static void getAllFoodListener() {
+        firebaseDatabase.getReference(FOOD)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot1) {
+
+                        ArrayList<Foods> arrayList = new ArrayList<>();
+
+                        for (DataSnapshot item : snapshot1.getChildren()) {
+                            Foods itemData1 = item.getValue(Foods.class);
+
+                            arrayList.add(itemData1);
+                        }
+
+                        Log.e("truongpa", "getAllCategory - Success");
+
+                        for (Foods foods : arrayList) {
+                            Log.e("truongpa", "" + foods.getId() + " - " + foods.getTitle());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.e("truongpa", "getAllCategory - Fail");
+                    }
+                });
+
+    }
+    public static void deleteFood(long id) {
+        firebaseDatabase.getReference(FOOD)
+                .child(String.valueOf(id))
+                .removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+
 
 }
--- app/src/main/java/com/app/orderfood/user/account/UserAccountFragment.java
+++ app/src/main/java/com/app/orderfood/user/account/UserAccountFragment.java
@@ -1,15 +1,26 @@
 package com.app.orderfood.user.account;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.app.orderfood.databinding.FragmentUserAccountBinding;
+import com.app.orderfood.fb.Utils;
+import com.app.orderfood.models.User;
+import com.bumptech.glide.Glide;
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class UserAccountFragment extends Fragment {
@@ -31,8 +42,32 @@ public class UserAccountFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //Code từ đây code đi
+        FirebaseDatabase.getInstance().getReference().child("user").child(Utils.getUID())
+                .addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Lấy dữ liệu từ dataSnapshot
+                    User user = dataSnapshot.getValue(User.class);
+                    String photoUrl = user.getLinkAvt();
+                    String name = user.getName();
+                    String address = user.getAddress();
+                    String phone = user.getPhone();
+                    Glide.with(requireContext()).load(photoUrl).into(binding.imgAvt);
+                    binding.tvname.setText("Name: " + name);
+                    binding.tvaddress.setText("Address: " + address);
+                    binding.tvphone.setText("Phone Number: " + phone);
 
+                } else {
+                    Log.e("long", "Nothing");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("long", "Fail");
+            }
+        });
     }
 
 }
